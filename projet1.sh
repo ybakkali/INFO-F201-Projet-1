9784bc7259cf06495b5d23eac93a4a4b11f03106
Yahya Bakkali <yahyabakkali@hotmail.com>
2018-11-23 22:29:39 +0100

    better

--- projet1.sh
+++ projet1.sh
@@ -5,6 +5,9 @@ echo $3
 echo $4
 
 PoolV2(){
+
+    # Vérifie que si le pool V1 existe
+
     if [ -d $1 ]
     then
       if [ -d $2 ]
@@ -13,10 +16,16 @@ PoolV2(){
       else
         mkdir $2
       fi
+      return 0
+    else
+      return 1
     fi
 }
 
 isUser(){
+
+  # Renvoie True si l'utilisateur existe sur la machine sinon False
+
   if id "$1" >/dev/null 2>&1; then
         echo "user exists"
         return 0
@@ -27,81 +36,92 @@ isUser(){
 }
 
 setRights() {
-  sudo chmod u=wrx,g=rx,o=- $1
+
+  # Fixe les permissions d'un fichier ou d'un dossier
+
+  chmod u=wrx,g=rx,o=- $1
 }
+
 setOwner() {
+
+  # Fixe récursivement le propriétaire et le groupe
+  # associé à un fichier ou à un dossier dans le pool V2
+
   for directory in $1/*
   do
   if [ -d $directory ]
   then
-      sudo chown $2:$3 $directory
+      chown $2:$3 $directory
       setOwner $directory $2 $3
       setRights $directory
   else
       currentDir=${directory##*v2/}
       dirName=${currentDir%%/*}
-      sudo chown $dirName:$3 $directory
+      chown $dirName:$3 $directory
       setRights $directory
   fi
   done
 }
 poolV1_to_poolV2(){
-    #For directory in pool v1
+
+    # Fonction qui permet la migration des fichiers du pool V1 vers le pool V2
+
+    # Pour chaque répertoire du pool v1
     for directory in $1/*
     do
     if [ -d $directory ]
     then
         poolV1_to_poolV2 $directory $2 $3 $4
     else
-        #Directory name
-        currentDir=${directory%/*}
-        dirName=${currentDir##*/}
+        dirPATH=${directory%/*}
+        # "PATH" vers le répertoire courant
+        dirName=${dirPATH##*/}
+        # Nom du répertoire personnel en cours de traitement
+
         if isUser $dirName
         then
             dirUSER=$dirName
         else
-            #Directory User
-            dirPATH=${directory%/*}
             dirLS=($(ls -ld $dirPATH ))
+            # Liste des informations sur le répertoire
             dirUSER=${dirLS[2]}
+            # Le propriétaire du répertoire personnel
         fi
 
-        #File
-        photo=${directory##*/}
-        photoDate=${photo%_*}
+        # Nom du fichier en cours de traitement
+        photoName=${directory##*/}
+        photoDate=${photoName%_*}
         year=$(date +%Y -d @$photoDate)
         month=$(date +%m -d @$photoDate)
         day=$(date +%d -d @$photoDate)
-        echo "file name :" $photo
+        echo "file name :" $photoName
 
-        #Create directories (year/month/day)
+        # Créer les répertoires (année/mois/jour)
         mkdir -p $2/$dirUSER/$year/$month/$day
 
-
         #Directories rights
-        #sudo chown $3:$4 $2/$dirUSER
-        #sudo chown $3:$4 $2/$dirUSER/$year
-        #sudo chown $3:$4 $2/$dirUSER/$year/$month
-        #sudo chown $3:$4 $2/$dirUSER/$year/$month/$day
+        #chown $3:$4 $2/$dirUSER $2/$dirUSER/$year $2/$dirUSER/$year/$month $2/$dirUSER/$year/$month/$day
         #setRights $2/$dirUSER $2/$dirUSER/$year $2/$dirUSER/$year/$month $2/$dirUSER/$year/$month/$day
 
-        #Copy the photo to the pool v2
         cp $directory $2/$dirUSER/$year/$month/$day
-        newPhotoName=${photo#*_}
-        mv $2/$dirUSER/$year/$month/$day/$photo $2/$dirUSER/$year/$month/$day/$newPhotoName
+        # Copier la photo dans le pool v2
+        newPhotoName=${photoName#*_}
+        mv $2/$dirUSER/$year/$month/$day/$photoName $2/$dirUSER/$year/$month/$day/$newPhotoName
+        # Renommer la photo
 
-        #sudo chown $dirUSER:$4 $2/$dirUSER/$year/$month/$day/$photo
-        #sudo chmod u=wrx,g=rx,o=- $2/$dirUSER/$year/$month/$day/$photo
+        #chown $dirUSER:$4 $2/$dirUSER/$year/$month/$day/$photoName
+        #chmod u=wrx,g=rx,o=- $2/$dirUSER/$year/$month/$day/$photoName
     fi
     done
 }
 main() {
-    PoolV2 $1 $2
-    poolV1_to_poolV2 $1 $2 $3 $4
-    setOwner $2 $3 $4
-    ls -l -R $2
-    sudo chown $3:$4 $2
-    sudo chmod u=wrx,g=rx,o=- $2
+    if PoolV2 $1 $2
+        poolV1_to_poolV2 $1 $2 $3 $4
+        setOwner $2 $3 $4
+        ls -l -R $2
+        chown $3:$4 $2
+        chmod u=wrx,g=rx,o=- $2
+    fi
 }
 
 main $1 $2 $3 $4
